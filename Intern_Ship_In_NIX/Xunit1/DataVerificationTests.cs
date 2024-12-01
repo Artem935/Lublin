591c358825a8b9bade90f215676b713507bcbc2e
mr-macintosh01 <mr.macintosh01@gmail.com>
2024-12-01 21:15:39 +0100

    Add the new Unit tests for display

---
+++ Intern_Ship_In_NIX/Xunit1/DataVerificationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.DisplayConsole;
+
+namespace Xunit1
+{
+    public class DataVerificationTests
+    {
+        [Fact]
+        public void CorrectDataInt_ShouldReturnValidInt()
+        {
+            // Arrange
+            var dataVerification = new DataVerification();
+            var input = new StringReader("5\n");
+            Console.SetIn(input);
+
+            // Act
+            var result = dataVerification.CorrectDataInt("Enter number: ");
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void CorrectDataString_ShouldReturnValidString()
+        {
+            // Arrange
+            var dataVerification = new DataVerification();
+            var input = new StringReader("TestString\n");
+            Console.SetIn(input);
+
+            // Act
+            var result = dataVerification.CorrectDataString("Enter string: ");
+
+            // Assert
+            Assert.Equal("TestString", result);
+        }
+    }
+}
---
+++ Intern_Ship_In_NIX/Xunit1/MainDisplayTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.DisplayConsole;
+
+namespace Xunit1
+{
+    public class MainDisplayTest
+    {
+        [Fact]
+        public void Menu_ShouldReturnSelectedOption()
+        {
+            // Arrange
+            var display = new MainDisplay();
+            var input = new StringReader("1\n");
+            Console.SetIn(input);
+
+            // Act
+            var result = display.Menu();
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+    }
+}
